d995c3a5a8558a5ae3432818b73b6f3f3821e2ee
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-06-23 18:38:33 +0300

    Feat: text chatt functional

--- sexbook/models.py
+++ sexbook/models.py
@@ -4,14 +4,17 @@ from datetime import datetime
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=300)
+    
 
     def __str__(self):
         return str(self.name)
+
 class Message(models.Model):
-    message = models.CharField(max_length=1000)
-    posted_on = models.DateTimeField(default=datetime.now)
+    value = models.CharField(max_length=1000)
+    date = models.DateTimeField(default=datetime.now)
     user = models.CharField(max_length=300)
-    room = models.CharField(max_length=300)
+    room = models.CharField(max_length=1000000)
+ 
 
     def __str__(self):
         return str(self.user)
